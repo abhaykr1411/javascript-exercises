2a71ea30383d12d0d9b1149a10c0bc844115bdc4
ak <actuallyabhay@gmail.com>
2024-08-10 15:36:45 +0530

    02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let combinedString = "";
+    if(num < 0) return "ERROR";
 
+    for(let i = 0; i < word.length; i++) combinedString += word;
+    return combinedString;
 };
 
 // Do not edit below this line
