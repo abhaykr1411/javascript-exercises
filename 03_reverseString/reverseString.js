29467313d4123f6d9e84fdfec1368499fbb88c8d
ak <actuallyabhay@gmail.com>
2024-08-10 15:42:10 +0530

    03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(sentence) {
+    let reversed = "";
+    for(let i = 0; i < sentence.length; i++) reversed += sentence[sentence.length - (i + 1)];
+    return reversed;
 };
 
 // Do not edit below this line
