118b8381b5d0bcfccf3fac40f1e3a5d7803028b9
ak <actuallyabhay@gmail.com>
2024-08-12 16:19:03 +0530

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+    let inArray = arguments[0];
+    for (let i = 1; i < arguments.length; i++)
+    {
+        while(inArray.includes(arguments[i])) inArray.splice(inArray.indexOf(arguments[i]), 1);
+    }
+      
+    return inArray;
 };
 
 // Do not edit below this line
