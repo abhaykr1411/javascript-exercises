a675fbb1c0be742880413c176f309133e52cff31
ak <actuallyabhay@gmail.com>
2024-08-13 23:22:20 +0530

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (!(Number.isInteger(num1) && Number.isInteger(num2))) return 'ERROR';
+    if (num1 < 0 || num2 < 0) return 'ERROR';
+    
+    let max = (num1 > num2)? num1 : num2;
+    let min = (num1 > num2)? num2 : num1;
+    
+    while(min <= max)
+    {
+        sum += min;
+        min++;
+    } 
+    return sum;
 };
 
 // Do not edit below this line
