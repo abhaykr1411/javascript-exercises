af9715131067275c2cd5d5fe9e486de77a333cb0
ak <actuallyabhay@gmail.com>
2024-08-13 23:57:15 +0530

    Completed till 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrVal) {
+  let cel = (fahrVal - 32) * (5/9); 
+  return Math.round(cel * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celVal) {
+  let fahr = (celVal * (9/5)) + 32;
+  return Math.round(fahr * 10) / 10;
 };
 
 // Do not edit below this line
